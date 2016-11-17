9d8838d44208339995f1e842b8fce6dc26808e0c
Brais Gabín <braisgabin@gmail.com>
2016-11-17 13:07:09 +0100

    Add some tests

---
+++ fragmenttestrule/src/test/java/com/android21buttons/fragmenttestrule/FragmentTestRuleTest.java
@@ -0,0 +1,40 @@
+package com.android21buttons.fragmenttestrule;
+
+import android.support.v4.app.Fragment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class FragmentTestRuleTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void createFragment() throws Exception {
+        FragmentTestRule<?, Fragment> testRule = FragmentTestRule.create(Fragment.class);
+
+        assertThat(testRule.createFragment(), is(notNullValue()));
+    }
+
+    @Test
+    public void getFragmentCreate() throws Exception {
+        FragmentTestRule<?, Fragment> testRule = FragmentTestRule.create(Fragment.class);
+
+        final Fragment fragment = testRule.createFragment();
+        assertThat(testRule.getFragment(), is(fragment));
+    }
+
+    @Test
+    public void getFragmentNoCreate() throws Exception {
+        FragmentTestRule<?, Fragment> testRule = FragmentTestRule.create(Fragment.class);
+
+        assertThat(testRule.getFragment(), is(nullValue()));
+    }
+}

db69407be6fcebffac1b6f550dbc3bf3e4107ca2
Brais Gabín <braisgabin@gmail.com>
2017-11-21 17:41:00 +0100

    Move sample from library to app

---
+++ sample/src/main/java/com/android21buttons/fragmenttestrule/sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android21buttons.fragmenttestrule.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
